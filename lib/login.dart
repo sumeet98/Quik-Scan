1d992d12e391a74397a74622ce7c6d8d9e97cfae
zainansari0417 <muhammad.ansari@uoit.net>
2019-11-02 18:19:49 -0400

    added navigationbar

--- lib/login.dart
+++ lib/login.dart
@@ -11,26 +11,49 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-
   bool _isLoggedIn = false;
 
   GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['email']);
 
-  _login() async{
-    try{
+  _login() async {
+    try {
       await _googleSignIn.signIn();
       setState(() {
         _isLoggedIn = true;
       });
-    } catch (err){
+    } catch (err) {
       print(err);
     }
   }
 
-  _logout(){
+  _logout() {
     _googleSignIn.signOut();
     setState(() {
       _isLoggedIn = false;
+    });
+  }
+
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Scanner',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Recent Scans',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: Settings',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
     });
   }
 
@@ -38,36 +61,65 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Welcome To Quik Scan", 
-        style: TextStyle(
-          fontWeight: FontWeight.w800,
-          color: Colors.white,
+        title: Text(
+          "Welcome To Quik Scan",
+          style: TextStyle(
+            fontWeight: FontWeight.w800,
+            color: Colors.white,
+          ),
         ),
-        ),
-      ), 
+      ),
       backgroundColor: Colors.indigo,
       body: Center(
-            child: _isLoggedIn
-                ? Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Image.network(_googleSignIn.currentUser.photoUrl, height: 50.0, width: 50.0,),
-                      Text(_googleSignIn.currentUser.displayName),
-                      OutlineButton( child: Text("Logout"), onPressed: (){
-                        _logout();
-                      },)
-                    ],
+        child: _isLoggedIn
+            ? Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Image.network(
+                    _googleSignIn.currentUser.photoUrl,
+                    height: 50.0,
+                    width: 50.0,
+                  ),
+                  Text(_googleSignIn.currentUser.displayName),
+                  OutlineButton(
+                    child: Text("Logout"),
+                    onPressed: () {
+                      _logout();
+                    },
                   )
-                : Center(
-                    child: OutlineButton(
-                      child: Text("Login with Google",),
-                      textColor: Colors.white,
-                      onPressed: () {
-                        _login();
-                      },
-                    ),
-                  )
-                  ), 
+                ],
+              )
+            : Center(
+                child: OutlineButton(
+                  child: Text(
+                    "Login with Google",
+                  ),
+                  textColor: Colors.white,
+                  onPressed: () {
+                    _login();
+                  },
+                ),
+              ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.camera_alt),
+            title: Text('Scanner'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            title: Text('Recent Scans'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            title: Text('Settings'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
