f83ceb0ed94258f9b8c971ea7b2f8a0f9501fbdf
zainansari0417 <muhammad.ansari@uoit.net>
2019-11-09 14:51:34 -0500

    Update main_menu.dart

--- lib/main_menu.dart
+++ lib/main_menu.dart
@@ -13,114 +13,105 @@ class MainMenu extends StatefulWidget {
 }
 
 class _MainMenuState extends State<MainMenu> {
-  
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.indigo,
-      body: Center(  
+      body: Center(
         child: Column(
-        children: <Widget>[  
-          Padding( 
-            padding: EdgeInsets.all(100),
-          ),
-          Image.network( 
-            'https://cdn2.iconfinder.com/data/icons/antivirus-internet-security/33/quick_scan-512.png',
-             height: 100, 
-             width: 200,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(75),
             ),
-
-            Text( 
+            Image.network(
+              'https://cdn2.iconfinder.com/data/icons/antivirus-internet-security/33/quick_scan-512.png',
+              height: 100,
+              width: 200,
+            ),
+            Text(
               "Quik Scan",
-               style: TextStyle(  
+              style: TextStyle(
                 color: Colors.white,
                 fontSize: 48,
                 fontWeight: FontWeight.w700,
-                )
-              ),
-
-              Padding( 
-                padding: EdgeInsets.all(100),
-              ),
-
-            FlatButton(
-              shape: new RoundedRectangleBorder(
-              borderRadius: new BorderRadius.circular(18.0),
-              side: BorderSide(color: Colors.white)),
-              color: Colors.indigo,
-              textColor: Colors.white,
-              padding: EdgeInsets.fromLTRB(107, 15, 107, 15),
-              onPressed: () {
-                _Login(context);
-              },
-              child: Text(
-                "Login".toUpperCase(),
-                style: TextStyle(
-                fontSize: 14.0,
               ),
             ),
-          ),
-
-          Padding( 
-            padding: EdgeInsets.all(10),
-          ),
-
-            FlatButton(
-              shape: new RoundedRectangleBorder(
-              borderRadius: new BorderRadius.circular(18.0),
-              side: BorderSide(color: Colors.white)),
-              color: Colors.indigo,
-              textColor: Colors.white,
-              padding: EdgeInsets.fromLTRB(100, 15, 100, 15),
-              onPressed: () {
-              },
-              child: Text(
-                "Sign Up".toUpperCase(),
-                style: TextStyle(
-                fontSize: 14.0,
-              ),
-            ),
-          ),
-
-            Padding( 
-            padding: EdgeInsets.all(10),
-          ),
-
-            FlatButton(
-              shape: new RoundedRectangleBorder(
-              borderRadius: new BorderRadius.circular(18.0),
-              side: BorderSide(color: Colors.white)),
-              color: Colors.indigo,
-              textColor: Colors.white,
-              padding: EdgeInsets.fromLTRB(25, 7, 25, 7),
-              onPressed: () {
-                _Page1(context);
-              },
-              child: Text(
-                "Skip".toUpperCase(),
-                style: TextStyle(
-                fontSize: 14.0,
-              ),
-            ),
-          ),
-
-
-
-        ],
-      )
-      )
-   
+            Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(75),
+                ),
+                FlatButton(
+                  shape: new RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(18.0),
+                      side: BorderSide(color: Colors.white)),
+                  color: Colors.indigo,
+                  textColor: Colors.white,
+                  padding: EdgeInsets.fromLTRB(107, 15, 107, 15),
+                  onPressed: () {
+                    _Login(context);
+                  },
+                  child: Text(
+                    "Login".toUpperCase(),
+                    style: TextStyle(
+                      fontSize: 14.0,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                ),
+                FlatButton(
+                  shape: new RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(18.0),
+                      side: BorderSide(color: Colors.white)),
+                  color: Colors.indigo,
+                  textColor: Colors.white,
+                  padding: EdgeInsets.fromLTRB(100, 15, 100, 15),
+                  onPressed: () {},
+                  child: Text(
+                    "Sign Up".toUpperCase(),
+                    style: TextStyle(
+                      fontSize: 14.0,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                ),
+                FlatButton(
+                  shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(18.0),
+                    side: BorderSide(color: Colors.white),
+                  ),
+                  color: Colors.indigo,
+                  textColor: Colors.white,
+                  padding: EdgeInsets.fromLTRB(25, 7, 25, 7),
+                  onPressed: () {
+                    _page1(context);
+                  },
+                  child: Text(
+                    "Skip".toUpperCase(),
+                    style: TextStyle(
+                      fontSize: 14.0,
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 
-    Future<void> _Login(BuildContext context) async {
+  Future<void> _Login(BuildContext context) async {
     var event = await Navigator.pushNamed(context, '/gotoLogin');
     print('gotoLogin:');
     print(event);
   }
 
-    Future<void> _Page1(BuildContext context) async {
+  Future<void> _page1(BuildContext context) async {
     var event = await Navigator.pushNamed(context, '/gotopage1');
     print('gotopage1:');
     print(event);
