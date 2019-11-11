074f5d941c452a7760ad8f5a92b226c6a72c89ee
Sumeet Dhillon <32371636+sumeet98@users.noreply.github.com>
2019-11-11 13:19:33 -0500

    login with email

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,20 @@
 import 'package:flutter/material.dart';
-import 'login.dart';
-import 'main_menu.dart';
-import 'page1.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'bottomNav.dart';
-import 'page2.dart';
-import 'page3.dart';
+import 'package:quik_scan/services/authentication.dart';
+import 'package:quik_scan/pages/root_page.dart';
 
-
-void main() => runApp(MyApp());
+void main() {
+  runApp(new MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Quik Scan',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-      ),
-      home: MainMenu(),
-    );
+    return new MaterialApp(
+        title: 'Welcome to Quik Scan',
+        debugShowCheckedModeBanner: false,
+        theme: new ThemeData(
+          primarySwatch: Colors.indigo,
+        ),
+        home: new RootPage(auth: new Auth()));
   }
 }
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:quik_scan/services/authentication.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'dart:async';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.auth, this.userId, this.logoutCallback})
+      : super(key: key);
+
+  final BaseAuth auth;
+  final VoidCallback logoutCallback;
+  final String userId;
+
+  @override
+  State<StatefulWidget> createState() => new _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  final FirebaseDatabase _database = FirebaseDatabase.instance;
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+
+  @override
+  void initState() {
+    super.initState();
+
+  }
+
+
+  signOut() async {
+    try {
+      await widget.auth.signOut();
+      widget.logoutCallback();
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body:
+      Column( 
+        children: <Widget>[  
+          Padding(
+            padding: EdgeInsets.all(15),
+          ),
+        InkWell(
+          // onTap: () => showDialog(  
+          //   context: context, 
+          //   builder: (BuildContext context) { 
+          //     return AlertDialog(  
+          //       title: Text('Options'),
+          //       content: new ListView(
+          //         children: <Widget>[  
+          //           new DropdownButton<String>(  
+          //             items: <String>['Logout'].map((String value) { 
+          //               return new DropdownMenuItem<String>(
+          //                 value: value,
+          //                 child: new Text(value),
+          //               );
+          //             }).toList(), 
+          //             onChanged: (_) {},
+          //           )
+          //         ],
+          //       ) ,
+          //     );
+          //   }
+          // ),
+          child: CircleAvatar(
+          backgroundImage: NetworkImage('https://icon-library.net/images/no-profile-picture-icon/no-profile-picture-icon-13.jpg'),
+          radius: 30,
+        ),
+      ),
+
+      FlatButton(
+        color: Colors.indigo,
+       child: new Text('Logout',
+        style: new TextStyle(fontSize: 17.0, color: Colors.white)),
+       onPressed: signOut)
+
+        ],
+      ) 
+
+    );
+  }
+}
