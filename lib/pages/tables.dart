ec8077e1b962a5d13dbee093ba92bab4169fb87f
Sumeet Dhillon <32371636+sumeet98@users.noreply.github.com>
2019-12-02 15:34:13 -0500

    tables and charts done

--- lib/pages/tables.dart
+++ lib/pages/tables.dart
@@ -5,6 +5,7 @@ import 'package:quik_scan/pages/home_page.dart';
 import 'package:quik_scan/main.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'dart:io';
+import 'package:charts_flutter/flutter.dart' as charts;
 
 class Tables extends StatefulWidget {
   Tables({Key key, this.auth, this.userId, this.logoutCallback})
@@ -14,6 +15,7 @@ class Tables extends StatefulWidget {
   final VoidCallback logoutCallback;
   final String userId;
 
+
   @override
   State<StatefulWidget> createState() => new _TablesState();
 }
@@ -22,6 +24,10 @@ class Tables extends StatefulWidget {
 
   final FirebaseDatabase _database = FirebaseDatabase.instance;
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+   Widget chartContainer = Column(  
+   mainAxisAlignment: MainAxisAlignment.center  
+ );
+
 
 
   @override
@@ -40,7 +46,7 @@ class Tables extends StatefulWidget {
 return Scaffold(
       appBar: AppBar(
         title: Text(
-          "Table",
+          "Tables",
           style: TextStyle(
             fontWeight: FontWeight.w800,
             color: Colors.white,
@@ -55,13 +61,135 @@ return Scaffold(
         automaticallyImplyLeading: false,
       ),
       backgroundColor: Colors.white,
-      body: ListView(
-        children: <Widget>[  
-          ListTile( 
-            title: Text('Tables', textScaleFactor: 3, textAlign: TextAlign.center,),
-          ),
-        ], 
-      ),
-    );
+     body: SingleChildScrollView(  
+         child: Column(  
+           children: <Widget>[  
+             Container(  
+               height: 250,  
+               child:  SimpleBarChart.withSampleData()  
+             ),  
+             Padding(  
+               padding: EdgeInsets.only(top: 10),  
+               child: Text('Coupons redeemed using QR codes in millions'),  
+             ),  
+                 Padding( 
+                   padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                 ),
+              Padding(  
+               padding: EdgeInsets.only(top: 10),  
+               child: Text('QR codes generated per day in millions'),  
+             ), 
+              Container(  
+               height: 250,  
+               child: SimpleLineGraph.withSampleData(),  
+             ),  
+               ],  
+             ),    
+         ),  
+       );    
   }
 }
+
+class SimpleBarChart extends StatelessWidget {  
+ final List<charts.Series> seriesList;  
+ final bool animate;  
+   
+ SimpleBarChart(this.seriesList, {this.animate});  
+     
+ factory SimpleBarChart.withSampleData() {  
+   return new SimpleBarChart(  
+     _createSampleData(),   
+     animate: false,  
+   );  
+ }  
+   
+ @override  
+ Widget build(BuildContext context) {  
+   return new charts.BarChart(  
+     seriesList,  
+     animate: animate,  
+      barRendererDecorator: new charts.BarLabelDecorator<String>(),
+   );  
+ }  
+   
+ 
+ static List<charts.Series<QrData, String>> _createSampleData() {  
+   final data = [  
+     new QrData('2016', 3.27),  
+     new QrData('2017', 3.37),  
+     new QrData('2018', 3.45),  
+     new QrData('2019', 3.51), 
+   ];  
+   
+   return [  
+     new charts.Series<QrData, String>(  
+       id: 'Qr',  
+       colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,  
+       domainFn: (QrData qr, _) => qr.year,  
+       measureFn: (QrData qr, _) => qr.houses,  
+       data: data,
+      labelAccessorFn: (QrData qr, _) =>
+      '${qr.houses}'  
+     )  
+   ];  
+ }
+}  
+
+class SimpleLineGraph extends StatelessWidget {  
+ final List<charts.Series> seriesList;  
+ final bool animate;  
+   
+ SimpleLineGraph(this.seriesList, {this.animate});  
+     
+ factory SimpleLineGraph.withSampleData() {  
+   return new SimpleLineGraph(  
+     _createSampleData(),   
+     animate: false,  
+   );  
+ }  
+   
+ @override  
+ Widget build(BuildContext context) {  
+   return new charts.LineChart(  
+     seriesList,  
+     animate: animate,  
+      defaultRenderer: new charts.LineRendererConfig(includePoints: true),
+   );  
+ }  
+   
+ 
+ static List<charts.Series<QrDataLine, int>> _createSampleData() {  
+   final data = [  
+     new QrDataLine(5, 30),  
+     new QrDataLine(85, 40),  
+     new QrDataLine(160, 50),  
+     new QrDataLine(240, 60), 
+   ];  
+   
+   return [  
+     new charts.Series<QrDataLine, int>(  
+       id: 'Qr',  
+       colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,  
+       domainFn: (QrDataLine qr, _) => qr.year,  
+       measureFn: (QrDataLine qr, _) => qr.codes,  
+       data: data,  
+     )  
+   ];  
+ }
+} 
+
+class QrData {  
+ final String year;  
+ final double houses;  
+   
+ QrData(this.year, this.houses);  
+}
+
+class QrDataLine {  
+ final int year;  
+ final double codes;  
+   
+ QrDataLine(this.year, this.codes);  
+}
+
+ 
